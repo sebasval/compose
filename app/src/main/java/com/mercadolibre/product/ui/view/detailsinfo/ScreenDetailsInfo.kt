94f05597cc0742843ff576a4471d4159d760373a
Sebastian Valentierra <s.dvh27@gmail.com>
2023-11-03 18:36:26 -0500

    fix ui details

--- app/src/main/java/com/mercadolibre/product/ui/view/detailsinfo/ScreenDetailsInfo.kt
+++ app/src/main/java/com/mercadolibre/product/ui/view/detailsinfo/ScreenDetailsInfo.kt
@@ -1,20 +1,27 @@
 package com.mercadolibre.product.ui.view.detailsinfo
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberAsyncImagePainter
 import com.mercadolibre.product.R
 import com.mercadolibre.product.ui.model.ProductItemInfo
 
 @Composable
-fun ScreenDetailsInfo(productItemInfo: ProductItemInfo){
+fun ScreenDetailsInfo(productItemInfo: ProductItemInfo) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -24,9 +31,35 @@ fun ScreenDetailsInfo(productItemInfo: ProductItemInfo){
         },
         modifier = Modifier.fillMaxSize()
     ) { padding ->
-        ProductInfo(productItemInfo = productItemInfo)
+
+        Column {
+            CarImage(item = productItemInfo)
+            Spacer(modifier = Modifier.height(8.dp))
+            ProductInfo(productItemInfo = productItemInfo)
+        }
     }
 }
+
+@Composable
+fun CarImage(item: ProductItemInfo) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = rememberAsyncImagePainter(item.thumbnailUrl),
+            contentDescription = stringResource(R.string.image_screen_title),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier
+                .fillMaxWidth(0.5f)
+                .aspectRatio(1f)
+                .clip(RoundedCornerShape(corner = CornerSize(16.dp)))
+        )
+    }
+}
+
 
 @Composable
 fun ProductInfo(productItemInfo: ProductItemInfo) {
